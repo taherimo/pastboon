8a83e3cb6d60f7bf77a7b6f8136247bd0e4fe7b1
Mohammad Taheri-Ledari <mo.taheri@ut.ac.ir>
2024-06-24 22:32:48 +0330

    Useless definitions were removed from boolean_network.h.

--- src/boolean_network.h
+++ src/boolean_network.h
@@ -6,53 +6,53 @@
 //#define SYMBOLIC_BOOLEAN_NETWORK 2
 
 
-typedef struct
-{
-  //unsigned char type;
-
-  unsigned int num_nodes;
-
-  int * fixed_nodes;
-  unsigned int * non_fixed_node_bits;
-
-  //int * node_types;
-
-  int * inputs;
-  int * input_positions;
-  int * outputs;
-  int * output_positions;
-  double * noise;
-  double * p00;
-  double * p01;
-  double * p10;
-  double * p11;
-  double * update_prob;
-
-} AsynchronousBooleanNetwork;
-
-
-typedef struct
-{
-  unsigned char type;
-
-  unsigned int num_nodes;
-
-  int * fixed_nodes;
-  unsigned int * non_fixed_node_bits;
-
-  //int * node_types;
-
-  int * inputs;
-  int * input_positions;
-  int * outputs;
-  int * output_positions;
-  double * noise;
-  double * p00;
-  double * p01;
-  double * p10;
-  double * p11;
-
-} SynchronousBooleanNetwork;
+// typedef struct
+// {
+//   //unsigned char type;
+//
+//   unsigned int num_nodes;
+//
+//   int * fixed_nodes;
+//   unsigned int * non_fixed_node_bits;
+//
+//   //int * node_types;
+//
+//   int * inputs;
+//   int * input_positions;
+//   int * outputs;
+//   int * output_positions;
+//   double * noise;
+//   double * p00;
+//   double * p01;
+//   double * p10;
+//   double * p11;
+//   double * update_prob;
+//
+// } AsynchronousBooleanNetwork;
+//
+//
+// typedef struct
+// {
+//   unsigned char type;
+//
+//   unsigned int num_nodes;
+//
+//   int * fixed_nodes;
+//   unsigned int * non_fixed_node_bits;
+//
+//   //int * node_types;
+//
+//   int * inputs;
+//   int * input_positions;
+//   int * outputs;
+//   int * output_positions;
+//   double * noise;
+//   double * p00;
+//   double * p01;
+//   double * p10;
+//   double * p11;
+//
+// } SynchronousBooleanNetwork;
 
 
 typedef struct
